83722d0e87b2ea2c5611903b3a0e67c0a31950a0
LabchariOussama <oussama.bx31@gmail.com>
2022-12-11 19:31:19 +0100

    m3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Fotter";
-import { Helmet } from "react-helmet";
+
 
 
 const setBodyColor = (color) => {
@@ -64,29 +64,6 @@ function App() {
           variants={content} className="App">
           <motion.header variants={itemsVariants}><CustomNavbar /></motion.header>
           <motion.main variants={itemsVariants}>
-            <Helmet>
-              {/* <!-- HTML Meta Tags --> */}
-              <title>sdfdsf</title>
-              <meta name="description" content="asdsadasd" data-react-helmet="true" />
-
-              {/* <!-- Google / Search Engine Tags --> */}
-              <meta itemprop="name" content="sdfdsf" data-react-helmet="true" />
-              <meta itemprop="description" content="asdsadasd" data-react-helmet="true" />
-              <meta itemprop="image" content="https://u-bx31.netlify.app/static/media/img_banner.f6b07343abbbd6eff1d5.png" data-react-helmet="true" />
-
-              {/* <!-- Facebook Meta Tags --> */}
-              <meta property="og:url" content="https://u-bx31.netlify.app/" data-react-helmet="true" />
-              <meta property="og:type" content="website" data-react-helmet="true" />
-              <meta property="og:title" content="sdfdsf" data-react-helmet="true" />
-              <meta property="og:description" content="asdsadasd" />
-              <meta property="og:image" content="https://u-bx31.netlify.app/static/media/img_banner.f6b07343abbbd6eff1d5.png" data-react-helmet="true" />
-
-              {/* <!-- Twitter Meta Tags --> */}
-              <meta name="twitter:card" content="summary_large_image" data-react-helmet="true" />
-              <meta name="twitter:title" content="sdfdsf" data-react-helmet="true" />
-              <meta name="twitter:description" content="asdsadasd" data-react-helmet="true" />
-              <meta name="twitter:image" content="https://u-bx31.netlify.app/static/media/img_banner.f6b07343abbbd6eff1d5.png" data-react-helmet="true" />
-            </Helmet>
             <Hero />
             <About />
             <Skills />
