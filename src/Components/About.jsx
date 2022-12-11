cbae6f64fe5337cfa74d8c20653303e23ad3b03d
LabchariOussama <oussama.bx31@gmail.com>
2022-12-11 19:59:04 +0100

    mdofie-aboutMe

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -34,15 +34,12 @@ const About = () => {
                     </div>
 
                 </ItemMotion>
-                <ItemMotion  className="c-about_desc p-lg-5 p-3">
+                <ItemMotion className="c-about_desc p-lg-5 p-3">
                     <p >
-                        👋 hi ! i'm Oussama Labchari. I am a Junoir web Devloper with passion and curosity on building fully responsive webSties and web applications
-                        , i realy like building web sites because it always made me learn something new,
-                        i've started learning and studying web developemnt over 2 years now , and also Still obtaining more information and knowledge
-                        about new and different Technologies that've been used recently.<br />
+                        👋 hi ! I'm Oussama Labchari. I am a Junior web Developer with a passion and curiosity for building fully responsive websites and web applications, 
+                        I really like building websites because it always makes me learn something new, I started learning and studying web development over 2 years now, and also Still obtaining more information and knowledge about new and different Technologies that've been used recently<br />
                         - <LocationIcon />  Tetouan , Morocco ;<br />
-
-                        - if You want to know more about my experience and my education. These is My <button className='btn_link c-underLinebroder' onClick={()=>handleOpen()}>"Resume"</button>
+                        - if You want to know more about my experience and my education. These is My <button className='btn_link c-underLinebroder' onClick={() => handleOpen()}>"Resume"</button>
                     </p>
                 </ItemMotion>
             </motion.div>
